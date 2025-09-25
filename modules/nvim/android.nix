ad18594715fb4f99b2512c513df53c5352353f82
Takuya-Mukai <takuya.muksi@gmail.com>
2025-09-25 23:35:57 +0900

    add android

---
+++ modules/nvim/android.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+
+{
+  imports = [
+    # nixCats-nvimが提供するhome-managerモジュールを読み込む
+    inputs.nixCats-nvim.homeModules.default
+  ];
+
+  # `nixCats`パッケージとそれに付随する設定を有効化
+  # flake.nixの `defaultPackageName = "nixCats";` に対応
+  nixCats = {
+    enable = true;
+    # インストールしたいパッケージの名前を指定
+    # あなたの `flake.nix` の `packageDefinitions` で定義されているものを指定します
+    packageNames = [ "androidCats" ];
+    # 他のモジュールオプションもここに追加できます
+    # 例えば、特定のカテゴリを上書きしたり、別のパッケージ名を有効化したり
+    # extraConfig = {
+    #   categories = {
+    #     go = true;
+    #   };
+    # };
+  };
+}
