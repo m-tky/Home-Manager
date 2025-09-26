28f43d5eeaacd7773956b4f87bdcf554de67ae09
Takuya-Mukai <takuya.muksi@gmail.com>
2025-09-26 16:32:17 +0900

    waybar update

--- modules/wayland/hypr/scripts/shutdown.sh
+++ modules/wayland/hypr/scripts/shutdown.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env zsh
-read -p "Shut down now? (y/n): " answer
 
-case "$answer" in
-    [Yy]* )
-        echo "Shutting down..."
-        shutdown now
-        ;;
-    [Nn]* )
-        echo "Canceled."
-        sleep 0.7
-        ;;
-    * )
-        echo "Invalid input. Use y or n."
-        sleep 0.7
-        ;;
-esac
+while true; do
+  printf "Shutdown? (y/n): "
+  read answer
+  case "$answer" in
+    [yY]) shutdown now ;;
+    [nN]) echo "Cancelled." && sleep 0.7 && break ;;
+    *)    echo "Invalid answer." && sleep 0.7 ;;
+  esac
+done
+
