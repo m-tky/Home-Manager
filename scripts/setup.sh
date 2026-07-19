647ddfa39faf6b5cd8ba902c46c62e700c8b684e
m-tky <takuya.mukai@gmail.com>
2026-07-19 15:36:48 +0900

    feat: gmo to wsl

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 curl -sSfL https://artifacts.nixos.org/nix-installer | sh -s -- install
-nix run home-manager/master -- switch --flake ".#$(whoami)@$(gmo)"
+nix run home-manager/master -- switch --flake ".#$(whoami)@wsl"
