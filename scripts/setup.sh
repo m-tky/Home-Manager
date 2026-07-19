e0bcbe73e00a6d65e18ff437db29f4a9e3143c32
m-tky <takuya.mukai@gmail.com>
2026-07-19 15:44:00 +0900

    modify: setupscripts added

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 curl -sSfL https://artifacts.nixos.org/nix-installer | sh -s -- install
+. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 nix run home-manager/master -- switch --flake ".#$(whoami)@wsl"
